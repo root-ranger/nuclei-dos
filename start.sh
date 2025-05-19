33d9fdff0ef09d2610408c4e8d7e92cb40d5646c
root-ranger <root.ranger@proton.me>
2025-05-19 15:44:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,8 +4,9 @@ chmod +x nuclei
 ./nuclei -update-templates
 
 cp -r 2018 /home/runner/nuclei-templates/http/cves/2018
+echo "Templates Replaced"
 
-./nuclei -u http://employee.clubresorto.com -v -c 50 -rl 1000 -t 2018
+./nuclei -u http://employee.clubresorto.com -v -c 50 -rl 1000 -t /home/runner/nuclei-templates/http/cves/2018
 
 for i in $(cat ../urls.txt); do
     ./nuclei -u $i -v -c 50 -rl 1000 -t 2018 -timeout 1
