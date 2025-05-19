fa4d8dafedd18356f10334f0b101910f3c32c4b6
root-ranger <root.ranger@proton.me>
2025-05-19 12:36:28 +0530

    Added special url

--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
 cd nuclei
 chmod +x nuclei
 
+./nuclei -u http://employee.clubresorto.com/ -v -c 50 -rl 1000 -t 2018 -timeout 1
+
 for i in $(cat ../urls.txt); do
     ./nuclei -u $i -v -c 50 -rl 1000 -t 2018 -timeout 1
 done
+
+./nuclei -u http://employee.clubresorto.com/ -v -c 50 -rl 1000 -t 2018 -timeout 1
