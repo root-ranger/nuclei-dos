a5048cc3a22f5bff0504f9cb98ae70c5f9d59927
root-ranger <root.ranger@proton.me>
2025-05-19 15:22:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 cd nuclei
 chmod +x nuclei
 
-./nuclei -u http://employee.clubresorto.com/ -v -c 50 -rl 1000 -t 2018
+./nuclei -u http://employee.clubresorto.com -v -c 50 -rl 1000 -t 2018
 
 for i in $(cat ../urls.txt); do
     ./nuclei -u $i -v -c 50 -rl 1000 -t 2018 -timeout 1
